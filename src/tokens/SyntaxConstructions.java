958852d6b14ca39dd016bb85bdae7342d23a2f0d
Ivan Petrov <79326021181@yandex.ru>
2023-10-22 03:23:58 +0500

    Добавлено README.md Добавлено исключение .idea в gitignore, всем нужно сделать также при работе с intellij

---
+++ src/tokens/SyntaxConstructions.java
@@ -0,0 +1,8 @@
+package src.tokens;
+
+/***
+ * Список токенов для определения синтаксических конструкций, пока только EOL, в будущем возможно расширим.
+ * */
+public enum SyntaxConstructions implements Tokens {
+    EOL // End Of Line - символизирует
+}
